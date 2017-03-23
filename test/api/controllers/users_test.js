2967c4a7fa58e9ccdff814ad3e6fd370bdb021f8
Mo-Sho <muhama94@gmail.com>
2017-03-22 23:48:59 -0700

    safety

--- test/api/controllers/users_test.js
+++ test/api/controllers/users_test.js
@@ -8,3 +8,74 @@ const bcrypt = require('bcrypt');
 const request = require('supertest');
 const knex = require('../../../knex');
 const server = require('../../../app');
+
+suite('users routes', () => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  beforeEach((done) => {
+    knex.seed.run()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('GET /users', (done) => {
+    request(server)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200,[
+        {
+          id: 1,
+          username: 'kDaddy',
+          first_name: 'Kevin',
+          last_name: 'Zheng'
+        },
+        {
+          id: 2,
+          username: 'MoSho',
+          first_name: 'Muhammad',
+          last_name:  'Shoman'
+        },
+        {
+          id: 3,
+          username: 'Makavelli',
+          first_name: 'Tupac',
+          last_name:  'Shakur'
+        },
+        {
+          id: 4,
+          username: 'BigBoi',
+          first_name: 'Big',
+          last_name:  'Boy'
+        },
+        {
+          id: 5,
+          username: 'MartyTheeMartian',
+          first_name: 'Marty',
+          last_name:  'Yee'
+        }
+      ],done)
+  });
+
+  test('POST /users', (done) => {
+    request(server)
+      .post('/users')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200,,done)
+  });
+});
