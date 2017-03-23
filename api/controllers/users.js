41a02f5fe4a32cebd2b8fa8516db771a399aca42
Mo-Sho <muhama94@gmail.com>
2017-03-22 17:47:43 -0700

    add post user controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,42 +1,66 @@
 'use strict';
 var util = require('util');
 var knex = require('../../knex.js');
+const bcrypt = require('bcrypt');
 
-
-function GetAllUsers(req, res){
-  return knex('users')
-    .then((users) => {
-      res.send(users)
-    })
-    .catch((err) => {
-      throw err;
-    });
+function GetAllUsers(req, res) {
+    return knex('users')
+        .select('id', 'first_name', 'last_name', 'username')
+        .then((users) => {
+            res.send(users);
+        })
+        .catch((err) => {
+            throw err;
+        });
 };
 
-function AddUser(){
+function AddUser(req, res) {
+    const { first_name, last_name, username, password } = req.body
+    return knex('users')
+        .where("username", username)
+        .then((users) => {
+                if (users[0]) {
+                    res.status(400).send("username already exists");
+                }
+                bcrypt.hash(password, 12)
+                    .then((hashed_password) => {
+                      const user = { first_name, last_name, username, hashed_password };
+                        delete user.password;
+                        return knex("users")
+                            .insert(user, '*')
+                    }).then((users) => {
+                        delete users[0].hashed_password;
+                        res.send(users[0]);
+                    })
+                    .catch((err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    });
 
-}
+            })
+        }
 
-function GetSpecificUser(){
+    function GetSpecificUser() {
 
-}
+    }
 
-function UpdateUser(){
+    function UpdateUser() {
 
-}
+    }
 
-function DeleteUser(){
+    function DeleteUser() {
 
-}
-
-
+    }
 
 
-module.exports = {
-  GetAllUsers: GetAllUsers,
-  AddUser: AddUser,
-  GetSpecificUser: GetSpecificUser,
-  UpdateUser: UpdateUser,
-  DeleteUser: DeleteUser
 
-};
+
+    module.exports = {
+        GetAllUsers: GetAllUsers,
+        AddUser: AddUser,
+        GetSpecificUser: GetSpecificUser,
+        UpdateUser: UpdateUser,
+        DeleteUser: DeleteUser
+
+    };
