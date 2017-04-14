5c04b9f5e2210972c21f3d29ba0515fbc227493f
Robert Murray <robertmurray@users.noreply.github.com>
2017-04-14 08:39:47 -0700

    Update restaurant_test.js
    Removed code that you aren't using.

--- test/api/controllers/restaurant_test.js
+++ test/api/controllers/restaurant_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 process.env.NODE_ENV = 'test';
-// const { suite, test } = require('mocha');
 const {describe,it} = require('mocha');
 const request = require('supertest');
 const knex = require('../../../knex.js');
@@ -45,19 +44,5 @@ describe('restaurant routes', () => {
       .expect('Content-Type', /json/)
        .expect(404, JSON.stringify({code:404, message: "please enter valid information"}, done));
   });
-  // test('GET restaurant/:id', (done) => {
-  //   request(server)
-  //     .get('/restaurant/3')
-  //     .set('Accept', 'application/json')
-  //     .set('Content-Type', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, {
-  //       id: 3,
-  //       name: "Gucci Mane Hotel",
-  //       street_name: "San Francisco",
-  //       city_name: "500 California St",
-  //       cost: 150,
-  //       date: "2017/12/12"
-  //     }, done)
-  // });
+
 });
