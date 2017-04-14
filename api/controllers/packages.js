5971fb57c53e4dab61f26cd20739af081aa1990a
Robert Murray <robertmurray@users.noreply.github.com>
2017-04-14 08:48:44 -0700

    Update packages.js
    take out the code that you aren't using.

--- api/controllers/packages.js
+++ api/controllers/packages.js
@@ -149,35 +149,6 @@ function PostUniquePackagePerUser(req, res) {
     })
 }
 
-// function GetUniquePackageUniqueUser(req, res, err) {
-//   return knex.from('users')
-//     .innerJoin('user_packages', 'users.id', 'user_packages.id')
-//     .join('flight_package', 'flight_package.package_id', 'user_packages.id')
-//     .join('flights', 'flights.id', 'flight_package.flight_id')
-//     .join('hotel_package', 'hotel_package.package_id', 'user_packages.id')
-//     .join('hotels', 'hotels.id', ' hotel_package.package_id')
-//     .join('restaurant_package', 'restaurant_package.package_id', 'user_packages.id')
-//     .join('restaurants', 'restaurants.id', 'restaurant_package.restaurant_id')
-//     .select('user_packages.id as package_id', 'users.id as user_id', 'airline', 'flights.id as flight_id', 'flights.cost as flight_cost', 'restaurants.name as restaurant_name', 'restaurants.id as restaurant_id',
-//       'restaurants.cost as restaurants_cost', 'hotels.name as hotels_name', 'hotels.id as hotels_id', 'hotels.cost as hotels_cost')
-//     .where('user_packages.id', req.swagger.params.package_id.value)
-//     .andWhere('user_id', req.swagger.params.user_id.value)
-//     .returning('*')
-//     .then((result) => {
-//       if (result) {
-//         console.log('what is result, kevin', result);
-//         res.set('Content-Type', 'application/json');
-//         res.send(result);
-//       } else {
-//         res.status(400);
-//         res.send('this is not a valid input');
-//         throw new Error("this end point doesn't exist");
-//       }
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     })
-// }
 
 module.exports = {
   GetAllPackagePerUser: GetAllPackagePerUser,
